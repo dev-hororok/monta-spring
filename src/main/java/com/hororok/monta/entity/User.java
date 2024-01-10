065128e731e1bbba5762578bca0f6ec11fc4d993
js1171 <bhd1171@naver.com>
2024-01-10 21:05:22 +0900

    :fire: 필요없는 import 삭제

--- src/main/java/com/hororok/monta/entity/User.java
+++ src/main/java/com/hororok/monta/entity/User.java
@@ -3,7 +3,6 @@ package com.hororok.monta.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;

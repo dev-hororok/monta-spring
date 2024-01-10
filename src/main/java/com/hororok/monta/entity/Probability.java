250b3b0d18ce090ced73cc4c596f93b7e7f0be18
HyeJiRoh <shgpwl509@gmail.com>
2024-01-10 17:47:09 +0900

    :sparkles: Probability Entity 생성

---
+++ src/main/java/com/hororok/monta/entity/Probability.java
@@ -0,0 +1,22 @@
+package com.hororok.monta.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Probability {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "probability_id")
+    private long id;
+
+    @NotNull
+    @Column(length = 10)
+    private String eggGrade; // SS, S+, S, A, B, C, AD
+
+    @NotNull
+    @Column(length = 10)
+    private String characterGrade; // SS, S+, S, A, B, C
+}

0d6d29dde535368f8c003291ee4c549c48786cd1
HyeJiRoh <shgpwl509@gmail.com>
2024-01-10 16:25:59 +0900

    :tada: 프로젝트 생성

---
+++ src/main/java/com/hororok/monta/MontaApplication.java
@@ -0,0 +1,13 @@
+package com.hororok.monta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MontaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MontaApplication.class, args);
+	}
+
+}

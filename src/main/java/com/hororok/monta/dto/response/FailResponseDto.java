3a225e8352027fff445761ed55606789f6458f00
HyeJiRoh <shgpwl509@gmail.com>
2024-01-13 00:12:09 +0900

    :art: 회원가입 기능 보완 및 응답 메세지 수정

--- src/main/java/com/hororok/monta/dto/response/FailResponseDto.java
+++ src/main/java/com/hororok/monta/dto/response/FailResponseDto.java
@@ -3,15 +3,17 @@ package com.hororok.monta.dto.response;
 import lombok.*;
 import org.springframework.http.HttpStatus;
 
+import java.util.Map;
+
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class FailResponseDto {
-
+    // 공통 에러 응답 Dto
     private int status;
     private String message;
-    private String errors;
+    private Map<String, String> errors;
 
 }

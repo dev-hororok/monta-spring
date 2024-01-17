b440cf23cce0de337fbfb1f43c4a980654291040
js1171 <bhd1171@naver.com>
2024-01-17 23:39:21 +0900

    :sparkles: Login 기능 추가 (Controller, Service)

--- src/main/java/com/hororok/monta/controller/AccountController.java
+++ src/main/java/com/hororok/monta/controller/AccountController.java
@@ -1,50 +1,57 @@
 package com.hororok.monta.controller;
 
-import com.hororok.monta.dto.request.AccountRequestDto;
-import com.hororok.monta.dto.response.FailResponseDto;
+import com.hororok.monta.dto.request.RegisterRequestDto;
+import com.hororok.monta.dto.request.LoginRequestDto;
 import com.hororok.monta.handler.CustomValidationException;
 import com.hororok.monta.jwt.TokenProvider;
 import com.hororok.monta.service.AccountService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class AccountController {
 
     private final AccountService accountService;
-//    private final TokenProvider tokenProvider;
-//    private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
-    public AccountController(AccountService accountService) {
+    public AccountController(AccountService accountService, TokenProvider tokenProvider, AuthenticationManagerBuilder authenticationManagerBuilder) {
         this.accountService = accountService;
-//        this.tokenProvider = tokenProvider;
-//        this.authenticationManagerBuilder = authenticationManagerBuilder;
     }
 
     @PostMapping("/auth/register")
-    public ResponseEntity<?> register(@Valid @RequestBody AccountRequestDto accountRequestDto, BindingResult bindingResult) {
+    public ResponseEntity<?> register(@Valid @RequestBody RegisterRequestDto registerRequestDto, BindingResult bindingResult) {
         if(bindingResult.hasErrors()){
-            Map<String, String> errors = new HashMap<>();
+            List<String> errors = new ArrayList<>();
 
-            for (FieldError error : bindingResult.getFieldErrors()){
-                errors.put(error.getField(), error.getDefaultMessage());
+            for(FieldError error : bindingResult.getFieldErrors()) {
+                errors.add(error.getField() + " : " + error.getDefaultMessage());
             }
-            throw new CustomValidationException("유효성 검사 실패", errors);
+            throw new CustomValidationException(errors);
         } else {
-            return accountService.saveMember(accountRequestDto);
+            return accountService.saveMember(registerRequestDto);
+        }
+    }
+
+    @PostMapping("/auth/login")
+    public ResponseEntity<?> login(@Valid @RequestBody LoginRequestDto loginRequestDto, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            List<String> errors = new ArrayList<>();
+
+            for(FieldError error : bindingResult.getFieldErrors()) {
+                errors.add(error.getField() + " : " + error.getDefaultMessage());
+            }
+            throw new CustomValidationException(errors);
+        } else {
+            return accountService.login(loginRequestDto);
         }
     }
 }

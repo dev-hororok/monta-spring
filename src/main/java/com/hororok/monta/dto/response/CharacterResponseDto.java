808958412ed1ae4be4aa4f6d9bda375668a21668
HyeJiRoh <shgpwl509@gmail.com>
2024-01-19 22:41:46 +0900

    :sparkles: CharacterResponseDto 생성

---
+++ src/main/java/com/hororok/monta/dto/response/CharacterResponseDto.java
@@ -0,0 +1,33 @@
+package com.hororok.monta.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+import org.springframework.http.HttpStatus;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CharacterResponseDto {
+    private int status;
+    private Data data;
+
+    public void setCharacterId(UUID characterId) {
+        if (this.data == null) {
+            this.data = new Data();
+        }
+        this.data.setCharacterId(characterId);
+        this.status = HttpStatus.CREATED.value();
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Data {
+        @JsonProperty("character_id")
+        private UUID characterId;
+    }
+}

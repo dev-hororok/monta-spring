a35c16d313fa73a6239bffc022001a085190cf20
HyeJiRoh <shgpwl509@gmail.com>
2024-01-22 14:33:27 +0900

    :art: StreakColorChangePermission @Setter 추가

--- src/main/java/com/hororok/monta/entity/StreakColorChangePermission.java
+++ src/main/java/com/hororok/monta/entity/StreakColorChangePermission.java
@@ -1,12 +1,13 @@
 package com.hororok.monta.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Getter
+@Setter
 public class StreakColorChangePermission extends CommonEntity{
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,5 +20,4 @@ public class StreakColorChangePermission extends CommonEntity{
     private Member member;
 
     private int availableChange;
-
 }

61d087674f902b8fd20218f5910c51e3ea7fb7b5
HyeJiRoh <shgpwl509@gmail.com>
2024-01-22 14:33:34 +0900

    :art: TransactionRecord @Setter 추가

--- src/main/java/com/hororok/monta/entity/TransactionRecord.java
+++ src/main/java/com/hororok/monta/entity/TransactionRecord.java
@@ -1,12 +1,13 @@
 package com.hororok.monta.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Getter
+@Setter
 public class TransactionRecord extends CommonEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)

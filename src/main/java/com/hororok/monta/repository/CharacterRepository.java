44a11223a516ee5517f9ecd9324f677ad99240cf
HyeJiRoh <shgpwl509@gmail.com>
2024-01-22 14:31:29 +0900

    :art: CharacterRepository GameCharacter 엔티티 이름 변경으로 인한 연계 수정

--- src/main/java/com/hororok/monta/repository/CharacterRepository.java
+++ src/main/java/com/hororok/monta/repository/CharacterRepository.java
@@ -1,6 +1,6 @@
 package com.hororok.monta.repository;
 
-import com.hororok.monta.entity.GameCharacter;
+import com.hororok.monta.entity.Character;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,10 +8,10 @@ import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface CharacterRepository extends JpaRepository<GameCharacter, UUID> {
+public interface CharacterRepository extends JpaRepository<Character, UUID> {
     boolean existsByName(String Name);
 
-    List<GameCharacter> findAll();
+    List<Character> findAll();
 
-    List<GameCharacter> findByGrade(String grade);
+    List<Character> findByGrade(String grade);
 }

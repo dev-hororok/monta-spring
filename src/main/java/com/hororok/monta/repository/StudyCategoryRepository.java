0f022d125c12ba494884669180011502cca06807
js1171 <bhd1171@naver.com>
2024-01-22 16:58:45 +0900

    :sparkles: 공부 시간 기록 위한 Repository 생성

---
+++ src/main/java/com/hororok/monta/repository/StudyCategoryRepository.java
@@ -0,0 +1,7 @@
+package com.hororok.monta.repository;
+
+import com.hororok.monta.entity.StudyCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudyCategoryRepository extends JpaRepository<StudyCategory, Long> {
+}

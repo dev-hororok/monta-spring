fd0c4968df33b926626afca7a53189839a095ce4
js1171 <bhd1171@naver.com>
2024-02-03 17:49:39 +0900

    :bug: palette 관련 코드 수정 (유효성, updated_at)

--- src/main/java/com/hororok/monta/dto/request/palette/PatchPaletteRequestDto.java
+++ src/main/java/com/hororok/monta/dto/request/palette/PatchPaletteRequestDto.java
@@ -12,25 +12,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class PatchPaletteRequestDto {
 
-    @NotBlank(message = "이름 필수 입력 값입니다.")
     private String name;
-
-    @NotNull(message = "유효하지 않은 등급이 입력되었습니다.")
     private String grade;
 
-    @NotBlank(message = "light_color 필수 입력 값입니다.")
     @JsonProperty("light_color")
     private String lightColor;
 
-    @NotBlank(message = "normal_color 필수 입력 값입니다.")
     @JsonProperty("normal_color")
     private String normalColor;
 
-    @NotBlank(message = "dark_color 필수 입력 값입니다.")
     @JsonProperty("dark_color")
     private String darkColor;
 
-    @NotBlank(message = "darker_color 필수 입력 값입니다.")
     @JsonProperty("darker_color")
     private String darkerColor;
 

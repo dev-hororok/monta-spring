bf15831a79c2fa6e4ed809a7b3313f43f84037fe
js1171 <bhd1171@naver.com>
2024-02-07 21:18:55 +0900

    :fire: 사용하지 않는 파일 삭제 (Nest에서 작업)

--- src/main/java/com/hororok/monta/repository/StudyCategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hororok.monta.repository;
-
-import com.hororok.monta.entity.StudyCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudyCategoryRepository extends JpaRepository<StudyCategory, Long> {
-}

d8443f19647442ad1252e97218aa7705ff3814c9
js1171 <bhd1171@naver.com>
2024-02-22 23:49:40 +0900

    :recycle: ResponseDto Code Convention 적용

--- src/main/java/com/hororok/monta/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/hororok/monta/jwt/JwtAuthenticationEntryPoint.java
@@ -6,7 +6,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
-
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -22,10 +21,7 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
                          HttpServletResponse response,
                          AuthenticationException authException) throws IOException {
         // 유효한 자격증명을 제공하지 않고 접근하려 할때 401 에러 반환
-        FailResponseDto failResponseDto = new FailResponseDto();
-        failResponseDto.setStatus("error");
-        failResponseDto.setMessage(Collections.singletonList("인증되지 않은 사용자의 접근입니다."));
-        failResponseDto.setError(HttpStatus.UNAUTHORIZED.name());
+        FailResponseDto failResponseDto = new FailResponseDto(HttpStatus.UNAUTHORIZED.name(), Collections.singletonList("인증되지 않은 사용자의 접근입니다."));
 
         response.setStatus(HttpStatus.UNAUTHORIZED.value());
         response.setContentType("application/json");

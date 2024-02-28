e3dd3946143ac8341547a4ed8afe35906565c316
js1171 <bhd1171@naver.com>
2024-02-28 22:24:15 +0900

    :art: 테스트 코드 위한 작업

--- src/test/java/com/hororok/monta/MontaApplicationTests.java
+++ src/test/java/com/hororok/monta/MontaApplicationTests.java
@@ -1,13 +1,13 @@
-//package com.hororok.monta;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class MontaApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package com.hororok.monta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MontaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

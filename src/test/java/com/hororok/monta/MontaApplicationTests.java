ea71e8e13cc8c8e52fd6efff53909887fbc6467c
js1171 <bhd1171@naver.com>
2024-03-12 12:42:26 +0900

    :white_check_mark: 테스트 환경 설정

--- src/test/java/com/hororok/monta/MontaApplicationTests.java
+++ src/test/java/com/hororok/monta/MontaApplicationTests.java
@@ -2,9 +2,15 @@ package com.hororok.monta;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("dev")
 class MontaApplicationTests {
+
+	@LocalServerPort
+	private int port;
 
 	@Test
 	void contextLoads() {

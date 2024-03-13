fff8f0e5a24a5125f73f18067df7ed8b53cd45bc
js1171 <bhd1171@naver.com>
2024-03-13 14:47:38 +0900

    :recycle: factory Pattern 적용

--- src/main/java/com/hororok/monta/service/ItemService.java
+++ src/main/java/com/hororok/monta/service/ItemService.java
@@ -8,11 +8,10 @@ import com.hororok.monta.dto.response.item.GetItemResponseDto;
 import com.hororok.monta.dto.response.item.GetItemsResponseDto;
 import com.hororok.monta.dto.response.item.UpdateItemResponseDto;
 import com.hororok.monta.dto.response.item.CreateItemResponseDto;
-import com.hororok.monta.dto.response.itemInventory.UseConsumableResponseDto;
-import com.hororok.monta.dto.response.itemInventory.UseFoodResponseDto;
 import com.hororok.monta.entity.*;
-import com.hororok.monta.entity.Character;
 import com.hororok.monta.repository.*;
+import com.hororok.monta.service.useItem.ItemUseStrategy;
+import com.hororok.monta.service.useItem.ItemUseStrategyFactory;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,19 +20,14 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import java.util.Random;
 
 @Service
 @AllArgsConstructor
 public class ItemService {
-
     private final ItemRepository itemRepository;
     private final ItemInventoryRepository itemInventoryRepository;
-    private final CharacterRepository characterRepository;
-    private final CharacterInventoryRepository characterInventoryRepository;
-    private final PaletteRepository paletteRepository;
-    private final StudyStreakRepository studyStreakRepository;
     private final MemberService memberService;
+    private final ItemUseStrategyFactory itemUseStrategyFactory;
 
     @Transactional
     public ResponseEntity<?> postItem(CreateItemRequestDto requestDto) {
@@ -139,125 +133,15 @@ public class ItemService {
                     .body(new FailResponseDto(HttpStatus.BAD_REQUEST.name(), Collections.singletonList("사용할 수 있는 수량이 없습니다.")));
         }
 
-        // 효과 번호 추출
-        int effectCode = itemInventory.getItem().getEffectCode();
-
-        // 10000번대일 (Food) 경우
-        if(effectCode>=10000 && effectCode<20000) {
-            // Progress가 0인지 체크
-            if(itemInventory.getProgress()>0) {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                        .body(new FailResponseDto(HttpStatus.BAD_REQUEST.name(), Collections.singletonList("잔여 시간만큼 공부해야 사용할 수 있습니다.")));
-            }
-            String characterGrade = characterGradeByEffect(effectCode);
-
-            // 존재하지 않는 효과의 경우 : 운영자 문의 요청
-            if(characterGrade.equals("Error")) {
-                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                        .body(new FailResponseDto(HttpStatus.INTERNAL_SERVER_ERROR.name()
-                                , Collections.singletonList("서버 오류 : 아이템 효과 없음 (운영자에게 문의해주세요)")));
-            }
-
-            // 구한 등급의 캐릭터 랜덤 뽑기
-            List<Character> gradeCharacterList = characterRepository.findByGrade(characterGrade);
-            if(gradeCharacterList.isEmpty()) {
-                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                        .body(new FailResponseDto(HttpStatus.INTERNAL_SERVER_ERROR.name(),
-                                Collections.singletonList("서버 오류 : 해당 등급의 캐릭터 없음 (운영자에게 문의해주세요.)")));
-            }
-            Character character = randomCharacterByGrade(gradeCharacterList);
-
-
-            // character Inventory 저장
-            Optional<CharacterInventory> findCharacterInventory = characterInventoryRepository.findOneByMemberIdAndCharacterId(member.getId(), character.getId());
-            CharacterInventory saveCharacterInventory;
-
-            if(findCharacterInventory.isEmpty()) {
-                CharacterInventory characterInventory = new CharacterInventory(member, character, 1);
-                saveCharacterInventory = characterInventoryRepository.save(characterInventory);
-            } else {
-                CharacterInventory characterInventory = findCharacterInventory.get();
-                characterInventory.updateQuantity(characterInventory.getQuantity()+1);
-                saveCharacterInventory = characterInventoryRepository.save(characterInventory);
-            }
-
-            // ItemInventory 수량 줄여 주고 soft delete
-            itemInventory.updateQuantity(0);
-            itemInventoryRepository.save(itemInventory);
-            itemInventoryRepository.delete(itemInventory);
-
-            return ResponseEntity.status(HttpStatus.OK)
-                    .body(new UseFoodResponseDto(saveCharacterInventory.getId(), character));
-        }
-
-        // 20000번대일 (Consumable) 경우
-        if(effectCode>=20000 && effectCode<30000) {
-
-            // 존재하지 않는 효과의 경우 : 운영자 문의 요청
-            Palette palette = consumableEffect(effectCode);
-            if(palette==null) {
-                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                        .body(new FailResponseDto(HttpStatus.INTERNAL_SERVER_ERROR.name()
-                                , Collections.singletonList("서버 오류 : 아이템 효과 없음 (운영자에게 문의해주세요.)")));
-            }
-
-            // studyStreak update (존재하지 않으면 새로 만들어주고, 존재하면 Palette update)
-            Optional<StudyStreak> findStudyStreak = studyStreakRepository.findByMemberId(member.getId());
-            if(findStudyStreak.isEmpty()) {
-                studyStreakRepository.save(new StudyStreak(palette, member));
-            }
-            else {
-                StudyStreak studyStreak = findStudyStreak.get();
-                studyStreak.updatePalette(palette);
-                studyStreakRepository.save(studyStreak);
-            }
-
-            // ItemInventory 수량 줄여 주기
-            itemInventory.updateQuantity(itemInventory.getQuantity()-1);
-            itemInventoryRepository.save(itemInventory);
-
-            return ResponseEntity.status(HttpStatus.OK)
-                    .body(new UseConsumableResponseDto(palette));
-        }
+        ItemUseStrategy strategy = itemUseStrategyFactory.getStrategy(itemInventory.getItem().getEffectCode());
 
         // 이외의 이상이 발생하는 경우
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new FailResponseDto(HttpStatus.INTERNAL_SERVER_ERROR.name()
-                        , Collections.singletonList("서버 오류 : 운영자에게 문의해주세요.")));
-    }
+        if(strategy==null) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new FailResponseDto(HttpStatus.INTERNAL_SERVER_ERROR.name()
+                            , Collections.singletonList("서버 오류 : 운영자에게 문의해주세요.")));
+        }
 
-
-    public String characterGradeByEffect(int effectCode) {
-        Random random = new Random();
-        double randomValue = random.nextDouble();
-
-        return switch (effectCode) {
-            case 10000 -> randomValue < 0.65 ? "C" : (randomValue < 0.95 ? "B" : "A");
-            case 10001 -> randomValue < 0.79 ? "C" : (randomValue < 0.99 ? "B" : "A");
-            case 10002 -> randomValue < 0.79 ? "B" : (randomValue < 0.99 ? "A" : "A+");
-            case 10003 -> randomValue < 0.90 ? "A" : "A+";
-            case 10004 -> randomValue < 0.90 ? "A+" : "S";
-            case 10005 -> randomValue < 0.90 ? "S" : "S+";
-            case 10006 -> randomValue < 0.90 ? "S+" : "SS";
-            default -> "Error";
-        };
-    }
-
-    public Character randomCharacterByGrade(List<Character> characterList) {
-        Random random = new Random();
-        return characterList.get(random.nextInt(characterList.size()));
-    }
-
-    public Palette consumableEffect(int effectCode) {
-        return switch (effectCode) {
-            case 20000 -> randomPaletteByEffect();
-            default -> null;
-        };
-    }
-
-    public Palette randomPaletteByEffect() {
-        Random random = new Random();
-        List<Palette> paletteList = paletteRepository.findAll();
-        return paletteList.get(random.nextInt(paletteList.size()));
+        return strategy.useItem(itemInventory, member);
     }
 }
